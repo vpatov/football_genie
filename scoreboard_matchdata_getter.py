11f96b6848a4e233286f23400d027e71eaf86e69
Vasia Patov <vasiapatov@gmail.com>
2016-12-10 16:10:05 -0500

    small changes to scoreboard_matchdata_getter.py

--- scoreboard_matchdata_getter.py
+++ scoreboard_matchdata_getter.py
@@ -23,8 +23,25 @@ def write_soup_to_file(soup,filename):
 
 
 
-test_match_stats_url = 'http://www.scoreboard.com/en/match/west-ham-arsenal-2016-2017/zRp4p5Xc/#match-summary|match-statistics;0|lineups;1'
-test_player_stats_url = 'http://www.scoreboard.com/en/match/west-ham-arsenal-2016-2017/zRp4p5Xc/#player-statistics;0'
+#test_match_stats_url = 'http://www.scoreboard.com/en/match/west-ham-arsenal-2016-2017/zRp4p5Xc/#match-summary|match-statistics;0|lineups;1'
+#test_player_stats_url = 'http://www.scoreboard.com/en/match/west-ham-arsenal-2016-2017/zRp4p5Xc/#player-statistics;0'
 
-test_soup = get_generated_dom(test_match_stats_url)
-write_soup_to_file(test_soup,'example_scoreboard_soup.txt')
+#test_soup = get_generated_dom(test_match_stats_url)
+#write_soup_to_file(test_soup,'example_scoreboard_soup.txt')
+
+test_batch_urls = []
+f = open('2010_2011_match_urls.txt','r')
+for line in f:
+	test_batch_urls.append(line.strip())
+
+i = 0
+for i in range(0,len(test_batch_urls)):
+	url = test_batch_urls[i]
+	#soup = get_generated_dom(url)
+	#write_soup_to_file(soup,'test' + str(i) + '.txt')
+	print "written", i
+	i += 1
+
+
+
+
